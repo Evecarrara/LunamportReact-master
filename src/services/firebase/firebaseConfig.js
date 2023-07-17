b43c6bd0f83b0131411465519ce804605a1f4767
luanacarrara <luanaevelyn99@gmai.com>
2023-07-16 21:26:51 -0300

    react

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,18 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore" // eslint-disable-line
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
+  apiKey: "AIzaSyDT7zf20WpVgcPB73_MzMYBvyaMoEXLxBw",
+  authDomain: "lunaimports-1fbbc.firebaseapp.com",
+  projectId: "lunaimports-1fbbc",
+  storageBucket: "lunaimports-1fbbc.appspot.com",
+  messagingSenderId: "340138482090",
+  appId: "1:340138482090:web:733bf6be99a46bb239b3d3",
+  measurementId: "G-DK2WN1RTDR"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // CARGAR PRODUCTOS AL COLLECTION
 
